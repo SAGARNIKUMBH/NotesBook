e92ca6a3eff9a4a73bcf9c6a717e47fcfd6c8a48
SAGAR_NIKUMBH <sagarnikumbh.weoto1991@gmail.com>
2021-12-31 17:26:29 +0530

    Add Form & Home page i note book

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
     <>
-      <Router>
-        <NavBar />
-        {/* <Home /> */}
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-        </Switch>
-      </Router>
+      <NoteState>
+        <Router>
+          <NavBar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </NoteState>
     </>
   );
 }
